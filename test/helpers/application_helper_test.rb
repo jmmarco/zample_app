cc382360793c497a1267c9bac0777db096694254
Juan Marco <jmmarco@gmail.com>
2019-06-28 09:52:11 -0300

    Finish layout and routes

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title,         'Skunk Works'
+    assert_equal full_title, 'Skunk Works'
+    assert_equal full_title("Sign up"), 'Sign up | Skunk Works'
     assert_equal full_title("Help"), 'Help | Skunk Works'
     assert_equal full_title("Contact"), 'Contact | Skunk Works'
     assert_equal full_title("About"), 'About | Skunk Works'
